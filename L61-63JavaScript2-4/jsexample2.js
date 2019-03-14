bbdfa44cf9c0f96c2e12e7cc5dfd023fce0c35f9
Mitrofanov Aleksey <mitrofanov_a@itstep.lan>
2019-03-14 21:40:43 +0300

    L63

--- L61-63JavaScript2-4/jsexample2.js
+++ L61-63JavaScript2-4/jsexample2.js
@@ -1,6 +1,6 @@
-//var a=0;
-//incrementTrice(a);
-//console.log(a);
+var a=0;
+incrementTrice(a);
+console.log(a);
 /*console.log(1=="1"); //true
 console.log(1==="1");//false
 console.log('1'=="1"); //true
@@ -15,8 +15,8 @@ console.log('1'!=="1");
 console.log(true!=1); 
 console.log(true!==1);*/
 
-//(2>3) ? "так не будет" : alert("paradox");
-//alert(2.71**Math.log(20));
+(2>3) ? "так не будет" : alert("paradox");
+alert(2.71*Math.log(20));
 
 //[] - myMap['человек1']; + просто как в джаве с массивами
 // + инициализация массива
@@ -28,12 +28,12 @@ console.log(true!==1);*/
 //	System.out.println(a)
 //}
 //JAVASCRIPT:
-//for (let a in arrayOfInt){
-//	console.log(a);
-//}
+for (let a in arrayOfInt){
+	console.log(a);
+}
 
-//console.log(2 in [33,222,2223]);
-//console.log(22 in [1,2,3]);
+console.log(2 in [33,222,2223]);
+console.log(22 in [1,2,3]);
 
 //управляющие конструкции:
 
@@ -45,15 +45,13 @@ c="v";
 d=undefined;
 [a,b,c,d]=[2, true, "v", undefined];
 
-//var array2=[for (i of numbers) i*2]; пока не стандарт
-
 
 function incrementTrice(chislo){
 	chislo = (chislo=chislo+1, chislo=chislo+1, chislo+1);
 	console.log(chislo);
 	
 	for (let i=(console.log("присваиваю и в " +
-			"начале цикла"), 0); i<10; i++) {
+	"начале цикла"), 0); i<("сравниваю с десяткой ", 10); i++) {
 		console.log(i);
 	}
 	
